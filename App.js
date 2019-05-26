61a0a9b3f707f0eec446c4746e7a7b39a311b9ef
frfontoura <frfontoura@gmail.com>
2019-05-26 10:56:00 -0300

    Criação do componente Post

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { View, Text } from "react-native";
 
 import Header from "./src/components/Header";
+import Post from "./src/components/Post";
 
 export default class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <View style={{ flex: 1 }}>
+        <Header />
+        <Post image={require("./assets/imgs/bw.jpg")} />
+      </View>
+    );
   }
 }
