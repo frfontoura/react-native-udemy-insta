ee4c642617cb20bd3817500faf423b3073674124
frfontoura <frfontoura@gmail.com>
2019-05-28 20:36:52 -0300

    Criação do component Comments

--- App.js
+++ App.js
@@ -6,10 +6,21 @@ import Post from "./src/components/Post";
 
 export default class App extends Component {
   render() {
+    const comments = [
+      {
+        nickname: "User Test 1",
+        comment: "bla bla bla"
+      },
+      {
+        nickname: "User Test 2",
+        comment: "qwerty!!!"
+      }
+    ];
+
     return (
       <View style={{ flex: 1 }}>
         <Header />
-        <Post image={require("./assets/imgs/bw.jpg")} />
+        <Post image={require("./assets/imgs/bw.jpg")} comments={comments} />
       </View>
     );
   }
