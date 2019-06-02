6dd15de8cfbb53997f33dfe502aea4cdb9b100ed
frfontoura <frfontoura@gmail.com>
2019-06-02 17:37:20 -0300

    Criação do componente Login

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { createBottomTabNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createSwitchNavigator
+} from "react-navigation";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 import Feed from "./screens/Feed";
 import AddPhoto from "./screens/AddPhoto";
 import Profile from "./screens/Profile";
+import Login from "./screens/Login";
+
+const loginOrProfileRouter = createSwitchNavigator(
+  {
+    Profile: Profile,
+    Auth: Login
+  },
+  {
+    initialRouteName: "Profile"
+  }
+);
 
 const MenuRoutes = {
   Feed: {
@@ -29,7 +43,7 @@ const MenuRoutes = {
   },
   Profile: {
     name: "Profile",
-    screen: Profile,
+    screen: loginOrProfileRouter,
     navigationOptions: {
       title: "Profile",
       tabBarIcon: ({ tintColor }) => (
---
+++ src/screens/Login.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput
+} from "react-native";
+
+export default class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  login = () => {
+    this.props.navigation.navigate("Profile");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+          autoFocus={true}
+          keyboardType="email-address"
+          value={this.state.email}
+          onChangeText={email => {
+            this.setState({ email });
+          }}
+        />
+        <TextInput
+          placeholder="Senha"
+          style={styles.input}
+          secureTextEntry={true}
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+        />
+        <TouchableOpacity onPress={this.login} style={styles.button}>
+          <Text style={styles.ButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.ButtonText}>Criar nova conta</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: "#4286f4"
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#FFF"
+  },
+  input: {
+    marginTop: 20,
+    width: "90%",
+    backgroundColor: "#EEE",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#333"
+  }
+});
