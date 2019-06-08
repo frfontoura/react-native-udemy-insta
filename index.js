2d2cbb4df1d2d5b5acb2ffef80f99b10588e8508
frfontoura <frfontoura@gmail.com>
2019-06-08 18:40:31 -0300

    Configuração do redux-thunk e firebase

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { name as appName } from "./app.json";
 import Navigator from "./src/Navigator";
 import storeConfig from "./src/store/storeConfig";
 
+import axios from "axios";
+axios.defaults.baseURL = "https://udemy-rn-6feea.firebaseio.com/";
+
 const store = storeConfig();
 const Redux = () => (
   <Provider store={store}>
